9632daeb20fe57e92f8f089bdc07c8d175f07850
Andrzej Gołaszewski <golaszewski.a@gmail.com>
2019-09-16 15:53:10 +0200

    wip

---
+++ src/PjoterParker.Api/Startup.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace PjoterParker.Api
+{
+    public class Startup
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+            _configuration = configuration.Build();
+
+            if (!hostingEnvironment.IsDevelopment())
+            {
+                _configuration = configuration.Build();
+            }
+
+            if (hostingEnvironment.IsDevelopment())
+            {
+                configuration.AddUserSecrets<Startup>();
+                _configuration = configuration.Build();
+            }
+
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc(routes => { });
+
+            //app.UseSwagger();
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pjoter Parker");
+            //    c.DisplayRequestDuration();
+            //});
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvc(configuration => { configuration.Filters.Add(typeof(ApiExceptionAttribute)); });
+            //services.AddDbContext<ApiDatabaseContext>();
+
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new Info { Title = "Pjoter Parker", Version = "v1" });
+            //    c.MapType<Guid>(() => new Schema { Type = "string", Format = "text", Description = "GUID" });
+            //    c.DescribeAllEnumsAsStrings();
+
+            //    c.CustomSchemaIds(x =>
+            //    {
+            //        int plusIndex = x.FullName.IndexOf("+");
+            //        int lastIndexOfDot = x.FullName.LastIndexOf(".");
+            //        int length = 0;
+
+            //        if (plusIndex != -1)
+            //        {
+            //            length = plusIndex - lastIndexOfDot - 1;
+            //        }
+            //        else
+            //        {
+            //            length = x.FullName.Length - lastIndexOfDot - 1;
+            //        }
+
+            //        return x.FullName.Substring(lastIndexOfDot + 1, length);
+            //    });
+            //});
+
+            //var applicationContainer = IocConfig.RegisterDependencies(services, _hostingEnvironment, _configuration);
+            //var cache = applicationContainer.Resolve<IServer>();
+            //cache.FlushDatabase();
+
+            return new AutofacServiceProvider(null);
+        }
+    }
+}
--- src/PjoterParker.EventStore/EventStoreRepository.cs
+++ src/PjoterParker.EventStore/EventStoreRepository.cs
@@ -1,123 +1,123 @@
-﻿//using System;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Autofac;
-//using EventStore.ClientAPI;
-//using Newtonsoft.Json;
-//using Newtonsoft.Json.Linq;
-//using PjoterParker.Core.Aggregates;
-//using PjoterParker.Core.Commands;
-//using PjoterParker.Core.Validation;
-//using StackExchange.Redis;
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using EventStore.ClientAPI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PjoterParker.Core.Aggregates;
+using PjoterParker.Core.Commands;
+using PjoterParker.Core.Validation;
+using StackExchange.Redis;
 
-//namespace PjoterParker.Core.EventStore
-//{
-//    public class EventStoreRepository : IAggregateStore
-//    {
-//        private const int READ_PAGE_SIZE = 500;
+namespace PjoterParker.Core.EventStore
+{
+    public class EventStoreRepository : IAggregateStore
+    {
+        private const int READ_PAGE_SIZE = 500;
 
-//        private const long WRITE_PAGE_SIZE = 500;
+        private const long WRITE_PAGE_SIZE = 500;
 
-//        private readonly IDatabase _cache;
+        private readonly IDatabase _cache;
 
-//        private readonly IComponentContext _context;
+        private readonly IComponentContext _context;
 
-//        private readonly IEventStoreConnection _eventStore;
+        private readonly IEventStoreConnection _eventStore;
 
-//        public EventStoreRepository(IEventStoreConnection eventStore, IDatabase cache, IComponentContext context)
-//        {
-//            _eventStore = eventStore;
-//            _cache = cache;
-//            _context = context;
-//        }
+        public EventStoreRepository(IEventStoreConnection eventStore, IDatabase cache, IComponentContext context)
+        {
+            _eventStore = eventStore;
+            _cache = cache;
+            _context = context;
+        }
 
-//        public async Task<TAggregate> GetByIdAsync<TAggregate>(Guid AggregateId) where TAggregate : AggregateRoot<TAggregate>, new()
-//        {
-//            return await GetByIdAsync<TAggregate>(AggregateId, int.MaxValue);
-//        }
+        public async Task<TAggregate> GetByIdAsync<TAggregate>(Guid AggregateId) where TAggregate : AggregateRoot<TAggregate>, new()
+        {
+            return await GetByIdAsync<TAggregate>(AggregateId, int.MaxValue);
+        }
 
-//        public async Task<TAggregate> GetByIdAsync<TAggregate>(Guid AggregateId, int version) where TAggregate : AggregateRoot<TAggregate>, new()
-//        {
-//            if (version <= 0)
-//            {
-//                throw new InvalidOperationException("Cannot get version <= 0");
-//            }
+        public async Task<TAggregate> GetByIdAsync<TAggregate>(Guid AggregateId, int version) where TAggregate : AggregateRoot<TAggregate>, new()
+        {
+            if (version <= 0)
+            {
+                throw new InvalidOperationException("Cannot get version <= 0");
+            }
 
-//            var streamName = $"{typeof(TAggregate).Name}:{AggregateId}";
-//            TAggregate aggregate = new TAggregate();
+            var streamName = $"{typeof(TAggregate).Name}:{AggregateId}";
+            TAggregate aggregate = new TAggregate();
 
-//            var value = await _cache.StringGetAsync(streamName);
-//            if (value.HasValue)
-//            {
-//                aggregate = JsonConvert.DeserializeObject<TAggregate>(value);
-//            }
+            var value = await _cache.StringGetAsync(streamName);
+            if (value.HasValue)
+            {
+                aggregate = JsonConvert.DeserializeObject<TAggregate>(value);
+            }
 
-//            long sliceStart = aggregate.Version + 1;
-//            StreamEventsSlice currentSlice = null;
+            long sliceStart = aggregate.Version + 1;
+            StreamEventsSlice currentSlice = null;
 
-//            do
-//            {
-//                int sliceCount = READ_PAGE_SIZE;
-//                currentSlice = await _eventStore.ReadStreamEventsForwardAsync(streamName, sliceStart, sliceCount, false);
+            do
+            {
+                int sliceCount = READ_PAGE_SIZE;
+                currentSlice = await _eventStore.ReadStreamEventsForwardAsync(streamName, sliceStart, sliceCount, false);
 
-//                if (currentSlice.Status == SliceReadStatus.StreamNotFound)
-//                {
-//                    throw new AppValidationException($"{typeof(TAggregate).Name}Id", $"Aggragate with id : {aggregate.Id} not found");
-//                }
+                if (currentSlice.Status == SliceReadStatus.StreamNotFound)
+                {
+                    throw new AppValidationException($"{typeof(TAggregate).Name}Id", $"Aggragate with id : {aggregate.Id} not found");
+                }
 
-//                if (currentSlice.Status == SliceReadStatus.StreamDeleted)
-//                {
-//                    throw new AppValidationException($"{typeof(TAggregate).Name}Id", $"Aggragate with id : {aggregate.Id} deleted");
-//                }
+                if (currentSlice.Status == SliceReadStatus.StreamDeleted)
+                {
+                    throw new AppValidationException($"{typeof(TAggregate).Name}Id", $"Aggragate with id : {aggregate.Id} deleted");
+                }
 
-//                sliceStart = currentSlice.NextEventNumber;
+                sliceStart = currentSlice.NextEventNumber;
 
-//                foreach (var evnt in currentSlice.Events)
-//                {
-//                    var realEvent = DeserializeEvent(evnt.OriginalEvent.Metadata, evnt.OriginalEvent.Data);
-//                    //((dynamic)aggregate).Apply((dynamic)realEvent);
-//                    aggregate.Version++;
-//                }
-//            }
-//            while (version >= currentSlice.NextEventNumber && !currentSlice.IsEndOfStream);
+                foreach (var evnt in currentSlice.Events)
+                {
+                    var realEvent = DeserializeEvent(evnt.OriginalEvent.Metadata, evnt.OriginalEvent.Data);
+                    ((dynamic)aggregate).Apply((dynamic)realEvent);
+                    aggregate.Version++;
+                }
+            }
+            while (version >= currentSlice.NextEventNumber && !currentSlice.IsEndOfStream);
 
-//            aggregate.Context = _context;
-//            return aggregate;
-//        }
+            aggregate.Context = _context;
+            return aggregate;
+        }
 
-//        public TAggregate GetNew<TAggregate>() where TAggregate : AggregateRoot<TAggregate>, new()
-//        {
-//            var model = new TAggregate();
-//            model.Context = _context;
-//            return model;
-//        }
+        public TAggregate GetNew<TAggregate>() where TAggregate : AggregateRoot<TAggregate>, new()
+        {
+            var model = new TAggregate();
+            model.Context = _context;
+            return model;
+        }
 
-//        public async Task SaveAsync(IAggregateRoot aggregate)
-//        {
-//            var streamName = $"{aggregate.GetType().Name}:{aggregate.Id}";
-//            var newEvents = aggregate.Events;
-//            var eventsToSave = newEvents.Select(e => ToEventData(e)).ToList();
+        public async Task SaveAsync(IAggregateRoot aggregate)
+        {
+            var streamName = $"{aggregate.GetType().Name}:{aggregate.Id}";
+            var newEvents = aggregate.Events;
+            var eventsToSave = newEvents.Select(e => ToEventData(e)).ToList();
 
-//            await _eventStore.AppendToStreamAsync(streamName, aggregate.Version, eventsToSave);
+            await _eventStore.AppendToStreamAsync(streamName, aggregate.Version, eventsToSave);
 
-//            aggregate.Version += aggregate.Events.Count();
-//            await _cache.StringSetAsync(streamName, JsonConvert.SerializeObject(aggregate), TimeSpan.FromDays(1));
-//        }
+            aggregate.Version += aggregate.Events.Count();
+            await _cache.StringSetAsync(streamName, JsonConvert.SerializeObject(aggregate), TimeSpan.FromDays(1));
+        }
 
-//        private object DeserializeEvent(byte[] metadata, byte[] data)
-//        {
-//            var eventType = JObject.Parse(Encoding.UTF8.GetString(metadata)).Property("EventType").Value;
-//            return JsonConvert.DeserializeObject(Encoding.UTF8.GetString(data), Type.GetType((string)eventType));
-//        }
+        private object DeserializeEvent(byte[] metadata, byte[] data)
+        {
+            var eventType = JObject.Parse(Encoding.UTF8.GetString(metadata)).Property("EventType").Value;
+            return JsonConvert.DeserializeObject(Encoding.UTF8.GetString(data), Type.GetType((string)eventType));
+        }
 
-//        private EventData ToEventData(EventComposite @event)
-//        {
-//            var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event.Event));
-//            var metadata = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event.Metadata));
-//            var typeName = @event.GetType().Name;
+        private EventData ToEventData(EventComposite @event)
+        {
+            var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event.Event));
+            var metadata = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event.Metadata));
+            var typeName = @event.GetType().Name;
 
-//            return new EventData(@event.Metadata.EventId, typeName, true, data, metadata);
-//        }
-//    }
-//}
+            return new EventData(@event.Metadata.EventId, typeName, true, data, metadata);
+        }
+    }
+}
